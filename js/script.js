f50a3b928c34e958a81cb38eed96395974842b47
all3x-martins <85321713+all3x-martins@users.noreply.github.com>
2025-01-25 22:24:05 -0300

    Adiciona página de login e faz ajustes na página inicial - Criação da página de login (login.html) com formulário para e-mail e senha. - Estilização do formulário de login para ser consistente com o design do site, incluindo campos de entrada e botão de login. - Implementação de um layout responsivo para a página de login, adaptando o conteúdo para dispositivos móveis. - Ajustes no menu de navegação da página inicial para garantir que o menu seja ocultado inicialmente e possa ser exibido ao clicar no ícone de hambúrguer. - Modificação da estrutura do menu de navegação, garantindo que a busca permaneça visível enquanto o menu é ocultado.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,5 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
-      this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0s ease forwards ${
-              index / 7 + 0.3
-            }s`);
-      });
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  const navList = document.getElementById('nav-list');
+  navList.classList.toggle('show'); // Alterna a visibilidade do menu
+});
   
