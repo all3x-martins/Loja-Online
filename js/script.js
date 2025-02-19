7b414fe61731ce0e2d3bcd035858b79cda1ba0b0
all3x-martins <85321713+all3x-martins@users.noreply.github.com>
2025-02-19 18:34:11 -0300

    feat: ajuste na exibição de preços, atualização de imagens e integração com JSON local
    - Atualização do código JavaScript para exibir valores monetários no formato brasileiro (R$ 2.576,51) utilizando `toLocaleString`.
    - Correção e otimização do carregamento dos produtos a partir do arquivo `produtos.json`.
    - Renomeação e padronização dos arquivos de imagens na pasta `assets/imagens`.
    - Estruturação aprimorada para renderização dinâmica dos cards de produtos.
    - Padronização do caminho `data/produtos.json` para leitura dos dados.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
  // Alterna a visibilidade do menu
- document.getElementById('menu-toggle').addEventListener('click', function() {
-  const navList = document.getElementById('nav-list');
+ document.getElementById('menu_toggle').addEventListener('click', function() {
+  const navList = document.getElementById('nav_list');
   navList.classList.toggle('show');
 });
   
@@ -12,3 +12,28 @@ document.querySelector('.newsletter_form').addEventListener('submit', function(e
       e.preventDefault();
   }
 });
+
+const precoFormatado = (valor) => {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  };
+
+// Função para carregar os produtos do JSON e gerar os cards
+fetch('data/produtos.json')
+  .then(response => response.json())
+  .then(produtos => {
+    const container = document.getElementById('card_container');
+    produtos.forEach(produto => {
+      const div = document.createElement('div');
+      div.classList.add('card');
+      div.innerHTML = `
+        <img src="${produto.imagem}" alt="${produto.nome}" loading="lazy">
+        <a href="#" class="product_link">
+          <h2>${produto.nome}</h2>
+          <span class="produto_preco">${produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+          <span class="produto_pagamento">à vista</span>
+        </a>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(error => console.error('Erro ao carregar os produtos:', error));
