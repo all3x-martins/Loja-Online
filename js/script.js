e714d88f05e3dc076db34c3a44db2d0268154b79
all3x-martins <85321713+all3x-martins@users.noreply.github.com>
2025-02-18 21:37:29 -0300

    feat: Adicionada validação de e-mail no formulário de newsletter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
+ // Alterna a visibilidade do menu
+ document.getElementById('menu-toggle').addEventListener('click', function() {
   const navList = document.getElementById('nav-list');
-  navList.classList.toggle('show'); // Alterna a visibilidade do menu
+  navList.classList.toggle('show');
 });
   
+// Form Validation
+document.querySelector('.newsletter_form').addEventListener('submit', function(e) {
+  let email = document.querySelector('.newsletter_input--email').value;
+  if (!email.includes('@')) {
+      alert('Please enter a valid email address');
+      e.preventDefault();
+  }
+});
